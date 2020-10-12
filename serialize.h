68c21775a96c5e024c58b60bb3e3e561954bb4d1
Mark Ney <mney@mediciventures.com>
2020-10-12 10:21:28 -0600

    updates protocol version to work with latest release

--- serialize.h
+++ serialize.h
@@ -68,7 +68,7 @@ class CAutoFile;
 static const unsigned int MAX_SIZE = 0x02000000;
 
 // RIP-5 (restricted assets)
-static const int PROTOCOL_VERSION = 70026;
+static const int PROTOCOL_VERSION = 70028;
 
 // Used to bypass the rule against non-const reference to temporary
 // where it makes sense with wrappers such as CFlatData or CTxDB
